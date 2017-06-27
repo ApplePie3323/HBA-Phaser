e20308545f3e2afed6f9b98f75b725db20caf5e3
ApplePie3323 <lmika3323@gmail.com>
2017-06-26 14:55:21 -1000

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,26 +1,82 @@
-function init(){
-	
-}
-
 function preload(){
 game.load.image('background', 'images/background.png');
 game.load.json('level:1', 'data/level01.json'); 
+game.load.image('ground', 'images/ground.png');
+game.load.image('grass:8x1', 'images/grass_8x1.png');
+game.load.image('grass:6x1', 'images/grass_6x1.png');
+game.load.image('grass:4x1', 'images/grass_4x1.png');
+game.load.image('grass:2x1', 'images/grass_2x1.png');
+game.load.image('grass:1x1', 'images/grass_1x1.png');
+game.load.image('hero', 'images/hero_stopped.png');
+}
+
+
+function move(direction){
+    hero.body.velocity.x = direction * 200;
+}
+
+
+function loadLevel(data) {
+	data.platforms.forEach(spawnPlatform, this);
+	spawnCharacters({hero: data.hero});
+};
+
+function spawnCharacters (data){
+    hero = game.add.sprite(data.hero.x, data.hero.y, 'hero');
+    hero.anchor.set(0.5, 0.5);
+    game.physics.enable(hero);
+    hero.body.collideWorldBounds = true;
 };
 
 function create(){
 game.add.image(0,0, 'background');
 loadLevel(this.game.cache.getJSON('level:1'));
+leftKey = game.input.keyboard.addKey(Phaser.Keyboard.LEFT);
+rightKey = game.input.keyboard.addKey(Phaser.Keyboard.RIGHT);
 }
 
 
-function loadLevel(data) {
-	console.log(data)
+
+function spawnPlatform(platform) {
+    game.add.sprite(platform.x, platform.y, platform.image);
 };
 
 function update(){
+handleInput();
 
 }
 
 
+function handleInput(){
+	if (leftKey.isDown) { // move hero left
+		move(-1);
+	}
+	else if (rightKey.isDown) { // move hero right
+		move(1)
+	}
+	else{
+		move(0);
+	}
+}
+
+
+function init(){
+	game.renderer.renderSession.roundPixels = true;
+}
+
+
+function move(direction){
+    hero.body.velocity.x = direction * 200;
+    if (hero.body.velocity.x < 0) {
+        hero.scale.x = -1;
+    }
+    else if (hero.body.velocity.x > 0) {
+        hero.scale.x = 1;
+    }
+}
+
+
+
+
 //Create a game state
-var game = new Phaser.Game(960, 600, Phaser.AUTO, 'game', {init: init, preload: preload, create: create, update: update});
+var game = new Phaser.Game(960, 600, Phaser.AUTO, 'game', {init: init, preload: preload, create: create, update: update});jhfjhgjgkhjhgk
