92b54b3ee8cfff88db892e4bd693688172701458
ApplePie3323 <lmika3323@gmail.com>
2017-06-29 14:53:12 -1000

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,97 +1,72 @@
+function init(){
+    
+}
+
 function preload(){
-game.load.image('background', 'images/background.png');
-game.load.json('level:1', 'data/level01.json'); 
-game.load.image('ground', 'images/ground.png');
-game.load.image('grass:8x1', 'images/grass_8x1.png');
-game.load.image('grass:6x1', 'images/grass_6x1.png');
-game.load.image('grass:4x1', 'images/grass_4x1.png');
-game.load.image('grass:2x1', 'images/grass_2x1.png');
-game.load.image('grass:1x1', 'images/grass_1x1.png');
-game.load.image('hero', 'images/hero_stopped.png');
-//game.load.audio('sfx:jump', 'audio/jump.wav');
-game.load.spritesheet('coin', 'images/coin_animated.png', 22, 22);
-}
+    game.load.image('background', 'images/AoTbackdrop.png');
+    game.load.json('level:1', 'data/level01.json');
+    //spawn platform sprites
+    game.load.image('ground', 'images/ground.png');
+    game.load.image('grass:8x1', 'images/grass_8x1.png');
+    game.load.image('grass:6x1', 'images/grass_6x1.png');
+    game.load.image('grass:4x1', 'images/grass_4x1.png');
+    game.load.image('grass:2x1', 'images/grass_2x1.png');
+    game.load.image('grass:1x1', 'images/grass_1x1.png');
 
+    // load the hero image
+    game.load.image('hero', 'images/download.png');
+    game.load.audio('sfx:jump', 'audio/jump.wav');
+    game.load.audio('sfx:coin', 'audio/coin.wav');
+    game.load.spritesheet('coin', 'images/download (1).png', 22, 22);
 
-function move(direction){
-    hero.body.velocity.x = direction * 200;
-
-}
-
-
-function loadLevel(data) {
-    platforms = game.add.group();
-	data.platforms.forEach(spawnPlatform, this);
-	spawnCharacters({hero: data.hero});
-    game.physics.arcade.gravity.y = 1200;
-    platforms = game.add.group();
-    data.platforms.forEach(spawnPlatform, this);
-        platforms = game.add.group();
-    spawnCharacters({hero: data.hero, spiders: data.spiders});  
-    data.coins.forEach(spawnCoin, this);
-};
-
-function spawnCharacters (data){
-    hero = game.add.sprite(data.hero.x, data.hero.y, 'hero');
-    hero.anchor.set(0.5, 0.5);
-    game.physics.enable(hero);
-    hero.body.collideWorldBounds = true;
-    game.physics.arcade.gravity.y = 1200;
 };
 
 function create(){
-game.add.image(0,0, 'background');
-loadLevel(this.game.cache.getJSON('level:1'));
-leftKey = game.input.keyboard.addKey(Phaser.Keyboard.LEFT);
-rightKey = game.input.keyboard.addKey(Phaser.Keyboard.RIGHT);
-upKey = game.input.keyboard.addKey(Phaser.Keyboard.UP);
+    game.add.image(0, 0, 'background');
+    sfxJump = game.add.audio('sfx:jump');
+    sfxCoin = game.add.audio('sfx:coin');
+    loadLevel(this.game.cache.getJSON('level:1'));
+    leftKey = game.input.keyboard.addKey(Phaser.Keyboard.LEFT);
+    rightKey = game.input.keyboard.addKey(Phaser.Keyboard.RIGHT);
+    upKey = game.input.keyboard.addKey(Phaser.Keyboard.UP);
     upKey.onDown.add(function(){
         jump();
-        sfxJump = game.add.audio('sfx:jump');
     });
 }
 
+function update(){
+    handleInput();
+    handleCollisions();
+}
 
+function loadLevel(data) {
+    console.log(data);
+    platforms = game.add.group();
+    coins = game.add.group();
+    data.platforms.forEach(spawnPlatform, this);
+    // spawn hero and enemies
+    spawnCharacters({hero: data.hero, spiders: data.spiders});  
+    // spawn important objects
+    data.coins.forEach(spawnCoin, this);
+    game.physics.arcade.gravity.y = 1200;
+};
 
 function spawnPlatform(platform) {
     game.add.sprite(platform.x, platform.y, platform.image);
     var sprite = platforms.create(platform.x, platform.y, platform.image);
     game.physics.enable(sprite);
-    sprite.body.allowGravity = false; 
-    sprite.body.immovable = true; 
-
-
+    sprite.body.allowGravity = false;
+    sprite.body.immovable = true;
 };
 
-function update(){
-    handleCollisions();
-    handleInput();
-
-}
-
-
-function handleCollisions(){
-   game.physics.arcade.collide(hero, platforms);
+function spawnCharacters (data) {
+    // spawn hero
+    hero = game.add.sprite(data.hero.x, data.hero.y, 'hero');
+    hero.anchor.set(0.6, 0.6);
+    //Make the main character use the physics engine for movement
+    game.physics.enable(hero);
+    hero.body.collideWorldBounds = true;
 };
-
-
-function handleInput(){
-	if (leftKey.isDown) { // move hero left
-		move(-1);
-	}
-	else if (rightKey.isDown) { // move hero right
-		move(1); 
-	}
-	else {
-		move(0);
-	}
-}; 
-
-
-function init(){
-	game.renderer.renderSession.roundPixels = true;
-}
-
 
 function move(direction){
     hero.body.velocity.x = direction * 200;
@@ -101,23 +76,46 @@ function move(direction){
     else if (hero.body.velocity.x > 0) {
         hero.scale.x = 1;
     }
-} 
+}
 
-
+function handleInput(){
+    if (leftKey.isDown) { // move hero left
+        move(-1);
+    }
+    else if (rightKey.isDown) { // move hero right
+        move(1);
+    } 
+    else {
+        move(0);
+    }
+}
+function handleCollisions(){
+   game.physics.arcade.collide(hero, platforms);
+   game.physics.arcade.overlap(hero, coins, onHeroVsCoin, null);
+};
 
 function jump(){
     var canJump = hero.body.touching.down;
+    //Ensures hero is on the ground or on a platform
     if (canJump) {
         hero.body.velocity.y = -600;
+        sfxJump.play();
     }
     return canJump;
 }
 
-
 function spawnCoin(coin) {
     var sprite = coins.create(coin.x, coin.y, 'coin');
     sprite.anchor.set(0.5, 0.5);
+    sprite.animations.add('rotate', [0, 1, 2, 1], 6, true); // 6fps, looped
+    sprite.animations.play('rotate');
+    game.physics.enable(sprite);
+    sprite.body.allowGravity = false;
 };
 
+function onHeroVsCoin(hero, coin){
+    sfxCoin.play();
+    coin.kill();
+};
 //Create a game state
 var game = new Phaser.Game(960, 600, Phaser.AUTO, 'game', {init: init, preload: preload, create: create, update: update});
